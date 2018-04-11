d381ef6a1c740eb4b50ac7dc152c5fc322785b59
Mohamed Saleem <mohsal.saleem@gmail.com>
2018-04-11 16:50:52 +0530

    A single host can add multiple peers

--- peer/src/main/java/moh/sal/peer/Runner.java
+++ peer/src/main/java/moh/sal/peer/Runner.java
@@ -2,6 +2,7 @@ package moh.sal.peer;
 
 import io.vertx.core.Vertx;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -10,28 +11,43 @@ import java.util.HashMap;
 public class Runner {
 
     public static HashMap<String, String> commandLineOptionsParser(String args[]) throws Exception {
-        HashMap<String, String> options = new HashMap<>();
+
+        System.out.println(Arrays.toString(args));
+
         if(args.length == 0) {
             throw new Exception("No options present.");
         }
-        String[] optionsString = args[0].split(",");
-        if(optionsString.length == 0) {
-            throw new Exception("No options present");
+        HashMap<String, String> options = new HashMap<>();
+
+        String[] host = args[0].split("=");
+        if(host.length < 2) {
+            throw new Exception("Malformed host options");
+        }
+        if(!host[0].equals("HOST")) {
+            throw new Exception("First option needs to be HOST");
         }
 
-        for (String string: optionsString) {
-            String[] option = string.split("=");
-            if(option.length < 2) {
-                throw new Exception("Malformed options");
+        options.put("HOST", host[1]);
+
+        if(args.length > 1) {
+            String[] peers = args[1].split("=");
+            if(peers.length < 2) {
+                throw new Exception("Malformed host options");
             }
-            options.put(option[0], option[1]);
+            if(!peers[0].equals("PEERS")) {
+                throw new Exception("Second option needs to be PEERS");
+            }
+
+            options.put("PEERS", peers[1]);
         }
+
         return options;
     }
 
     public static void main(String[] args) {
         try {
             HashMap<String, String> options = commandLineOptionsParser(args);
+            System.out.println("options");
             Vertx vertx = Vertx.vertx();
             vertx.deployVerticle(new Peer(options));
         } catch (Exception e) {
